c97067fa8b03c0963c429704370a285887bfc314
= <adeel.khan@tkxel.io>
2022-04-19 23:03:19 +0500

    schlei_erleben

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = "http://ec2-44-201-188-15.compute-1.amazonaws.com/api/v1";
+// const BASE_URL = "http://ec2-44-201-188-15.compute-1.amazonaws.com/api/v1";
 // const BASE_URL = "https://fed3-180-178-189-110.ngrok.io/api/v1";
+const BASE_URL = "http://backend.renso.de/api/v1";
 const PAGE_SIZE = 10;
 
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 axios.defaults.headers.common.Authorization =
-  "c45af3ea8ad38c4f8cbc900133cb432c";
+  "331c244176cb02a86dbe044993189e95";
 
 function configureApis(Vue) {
   Vue.prototype.$extractErrorMsg = function (
