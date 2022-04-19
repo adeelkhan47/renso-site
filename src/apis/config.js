3b30fa28eb98c5b4ac64c4d7ffc37ec9631498b8
= <adiiiiikhan1998@outlook.com>
2022-04-20 00:48:13 +0500

    Rad settings

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "http://ec2-44-201-188-15.compute-1.amazonaws.com/api/v1";
+const BASE_URL = "http://backend.renso.de/api/v1";
 // const BASE_URL = "https://fed3-180-178-189-110.ngrok.io/api/v1";
 const PAGE_SIZE = 10;
 
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 axios.defaults.headers.common.Authorization =
-  "c45af3ea8ad38c4f8cbc900133cb432c";
+  "baf56a0e58f5c13d83c31c0b2d89b3ed";
 
 function configureApis(Vue) {
   Vue.prototype.$extractErrorMsg = function (
