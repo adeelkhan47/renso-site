4d0ef602260312bc8754061e5b3752384b95f7a6
Shahroz Saleem <shahrozsaleem17@gmail.com>
2022-04-29 21:45:03 +0500

    C5.12: Disabled time-picker based on item config

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const BASE_URL = "https://backend.renso.de/api/v1";
-// const BASE_URL = "https://fed3-180-178-189-110.ngrok.io/api/v1";
+// const BASE_URL = "https://00c4-180-178-189-110.ngrok.io/api/v1";
 const PAGE_SIZE = 10;
 
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 axios.defaults.headers.common.Authorization =
-  "c45af3ea8ad38c4f8cbc900133cb432c";
+  "3b81719d3a3f12b4f342ae06f1115b86";
 
 function configureApis(Vue) {
   Vue.prototype.$extractErrorMsg = function (
