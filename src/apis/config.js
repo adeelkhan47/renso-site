f4d2f290506ae4ed6ea21b54468f0b5eb48b294d
= <adiiiiikhan1998@outlook.com>
2022-04-29 23:08:24 +0500

    error fixed

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,11 +1,8 @@
 import axios from "axios";
-
 const BASE_URL = "https://backend.renso.de/api/v1";
 const PAGE_SIZE = 10;
-
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 axios.defaults.headers.common.Authorization = "baf56a0e58f5c13d83c31c0b2d89b3ed";
-
 
 function configureApis(Vue) {
   Vue.prototype.$extractErrorMsg = function (
