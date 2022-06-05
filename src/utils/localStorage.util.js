2322661898dd32a86a6fedebc9471786a8cc45d4
= <adiiiiikhan1998@outlook.com>
2022-06-05 10:34:14 +0200

    keys

--- src/utils/localStorage.util.js
+++ src/utils/localStorage.util.js
@@ -2,9 +2,8 @@ const storageKey = "renso_app_data";
 const EXISTING_CART_ID_KEY = "existingCartId";
 const APPLIED_VOUCHER_KEY = "appliedVoucher";
 const TRANSACTION_ID_KEY = "existingTransactionId";
-const STRIPE_PUBLIC_KEY =
-  "pk_test_51KK1AdInECVV2DaUUVrSfXyAr5Ee0RUQdNDCH1KeO6BjvXmhLVDx7sDROv0Vb49yFMC2q0q9CVYixtLcgmfWUTPp000jv4ibaT";
-
+const STRIPE_PUBLIC_KEY = "pk_live_51KhBNIE2epVqKFExgm8VrKkUhIeShgrM1t6fAB4AYB7GPENwquBOFBz1rj4ZD0eaSstWQWXJNf1e1sfNG7dr4VfA00mjIONJg8";
+// const STRIPE_PUBLIC_KEY = "pk_test_51KK1AdInECVV2DaUUVrSfXyAr5Ee0RUQdNDCH1KeO6BjvXmhLVDx7sDROv0Vb49yFMC2q0q9CVYixtLcgmfWUTPp000jv4ibaT";
 const MATCH_REGEX = Object.freeze({
   STARTS_WITH: "startsWith"
 });
